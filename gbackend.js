cd82ce8e6d16630924cc2eacfd33a8442171d962
Lionel <fullstackbeaver@gmail.com>
2021-12-01 11:47:06 +0100

    dernier version qui semble bien

--- gbackend.js
+++ gbackend.js
@@ -3,7 +3,6 @@ const server = require("./lib/server");
 module.exports={
   getMiddlewares     : server.getMiddlewares,
   listen             : server.listen,
-  // page               : require("./lib/page"),
   set                : server.setConfiguration,
   useMiddlewares     : server.setMiddlewares
 };
